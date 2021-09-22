67399ef2c23a960accac636ffdd50964f29f5a80
Mark Glants <mark.glants@lamoda.ru>
2021-09-22 16:58:32 +0300

    Added yandex cloud setup

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-curl https://storage.yandexcloud.net/yandexcloud-yc/install.sh | bash
+curl https://storage.yandexcloud.net/yandexcloud-yc/install.sh | bash -s -- -i ${HOME}/.local -n
+${HOME}/.local/bin/yc completion zsh > ~/.oh-my-zsh/custom/yc.zsh
