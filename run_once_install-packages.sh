2536163573430fba51bc78a0a6f9adff0d48b5fa
mark.glants <mark.glants@lamoda.ru>
2021-11-15 04:26:30 +0300

    Added for fzf

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 curl https://storage.yandexcloud.net/yandexcloud-yc/install.sh | bash -s -- -i ${HOME}/.local -n
 ${HOME}/.local/bin/yc completion zsh > ~/.oh-my-zsh/custom/yc.zsh
+$(brew --prefix)/opt/fzf/install
